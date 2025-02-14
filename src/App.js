83f3e9e2fb84fe6f6ca9f9eae10da7b3d608333b
Danilo Santos <danilo@devstudio.com.brm>
2025-02-13 23:02:42 -0300

    add function search cep

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
+import { useState } from "react"; 
 import { FiSearch } from "react-icons/fi";
 import './styles.css';
 
+import api from "./services/api";
+
 
 function App() {
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState('');
+
+  async function handleSearch() {
+
+    if(input === '') {
+      alert('Digite um CEP válido');
+      return;
+    }
+
+    try { 
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput('');
+    }catch {
+      alert('Erro na busca');
+      setInput('');
+    }
+  }
+
+
+
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
 
       <div className="containerInput"> 
-        <input type="text" placeholder="Digite o CEP" />
-        <button className="buttonSearch"><FiSearch size={25} color="#FFF"/></button>
+        <input 
+        type="text"
+        placeholder="Digite o CEP"
+        value={input}
+        onChange={(click) => setInput(click.target.value)} 
+      />
+
+      <button className="buttonSearch" onClick={handleSearch}><FiSearch size={25} color="#FFF"/></button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 37832-742</h2>
-
-        <span>Rua: Teste da Silva</span>
-        <span>Complemento: Sem Complemento</span>
-        <span>Bairro: Bairro dos bobos</span>
-        <span>Campo Grande - MG</span>
-        
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade}/{cep.uf}</span>
+        </main>
+      )}
     </div>
     
   );
